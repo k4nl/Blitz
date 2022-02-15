ac2705c9c4dc041320a1995eca4110b8f3ae6d85
k4nl <gustavoh.bh@gmail.com>
2022-02-14 23:23:53 -0300

    criando rotas e paginas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Sign from './pages/Sign';
 
 function App() {
   return (
-    <div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="sign" element={<Sign />} />
+    </Routes>
   );
 }
 
